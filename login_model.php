a826b45cd61e215e2a720208214c5498889a2286
Gregory Wiltshire <mellon@Gregorys-MacBook-Pro.local>
2018-12-07 20:43:19 -0500

    images working

--- login_model.php
+++ login_model.php
@@ -2,24 +2,6 @@
 
   include 'database.php';
 
-  
-  // function get_db_connection(){
-  //   $servername = "localhost";
-  //   $username = "root";
-  //   $password = "root";
-  //   $dbname = "rentaldb";
-
-  //   // Create connection
-  //   $conn = new mysqli($servername, $username, $password, $dbname);
-
-  //   // Check connection
-  //   if (!$conn) {
-  //       die("Connection failed: " . mysqli_connect_error());
-  //   }
-
-  //   return $conn;
-  // }
-
   function create_user($username,$password,$email){
     $conn = get_db_connection();
 
