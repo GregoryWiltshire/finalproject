d27ff78cc1f2e77392a38d95a40f359b0fcad471
rashad <rashad@rashads-MacBook-Pro.local>
2018-12-08 10:49:00 -0500

    updated cart

--- cart.php
+++ cart.php
@@ -16,7 +16,7 @@
 	$query = 'SELECT * FROM products ORDER BY PRICE DESC';
 	$table = mysqli_query($conn,$query);
 	$conn->close();
-
+      function generate(){
 		if($table){
 			if(mysqli_num_rows($table)>0){
 				while($items=mysqli_fetch_assoc($table)){
@@ -27,11 +27,11 @@
 									echo"<form method=post action=cart.php>";
 										echo "<h3 class=text-danger>$items[name]</h3>";
 											echo "<img src=$items[image]>";
-										echo"<h3>$items[price]></h3>";
+										echo"<h3>$items[price]</h3>";
 										echo "<input type=text name=quantity class=form-control value=1>";
 										echo "<input type=hidden name=name value=$items[name]>";
 										echo "<input type=hidden name=price value=price $items[price]>";
-										echo "<input type=submit name=additem value=Rent class=btn btn-dark style=margin-top: 10px>";
+										echo"<input type=submit name=additem value=Rent class=btn btn-dark style=margin-top: 10px>";
 									echo "</form>";
 								echo"</div>";
 						   echo "</div>";
@@ -41,6 +41,7 @@
 				}
 			}
 		}
+	}
 	?>
 	
 
