9499bfa032b471a704392c2b33e3135c00a91d0e
Gregory Wiltshire <mellon@Gregorys-MacBook-Pro.local>
2018-12-07 21:50:37 -0500

    remove images, added codd settings

--- database.php
+++ database.php
@@ -1,9 +1,10 @@
 <?php
+    //codd settings
     function get_db_connection(){
         $servername = "localhost";
-        $username = "root";
-        $password = "root";
-        $dbname = "rentaldb";
+        $username = "gwiltshire2";
+        $password = "gwiltshire2";
+        $dbname = "gwiltshire2";
 
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,5 +16,24 @@
 
         return $conn;
       }
+
+    //localhost settings
+
+    // function get_db_connection(){
+    //     $servername = "localhost";
+    //     $username = "root";
+    //     $password = "root";
+    //     $dbname = "rentaldb";
+
+    //     // Create connection
+    //     $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //     // Check connection
+    //     if (!$conn) {
+    //         die("Connection failed: " . mysqli_connect_error());
+    //     }
+
+    //     return $conn;
+    //}
 
   ?>
