4da8a7143bfa325db0acb63f4822ea3b11677434
rashad <rashad@rashads-mbp.attlocal.net>
2018-12-08 23:39:13 -0500

    checkout page

--- checkout.php
+++ checkout.php
@@ -1,12 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Cart</title>
+		  <style type="text/css">
+				table{
+				border: 1px solid black;
+		        table-layout: fixed;
+		        width: 200px;
+		      }
+				th,td{
+					border: 1px solid black;
+		            width: 100px;
+		            text-align: center;
+				}
+				tr {
+				    border-bottom: 1px solid black;
+				    border-top: 1px solid black;
+				    border-collapse: collapse;
+				}
+			</style>
+	</head>
+	<body>
+
+	</body>
+	</html>
+	
+	<?php
+	session_start();
+
+		$cart=$_SESSION	['cart'];
+		echo"<table border=1 width=500 height=500 rules=none>";
+		echo"<th>Car Rented</th>";
+		echo"<th>Car Price</th>";
+		echo"<th>Number of Days</th>";
+		echo"<th>Rental Start/End Days</th>";
+		foreach ($cart as $rows => $cart_item){
+		
+			echo "<tr>";
+			echo "<td>$cart_item[car_name]</td>";
+			echo "<td>$cart_item[price]</td>";
+			echo "<td>$cart_item[days]</td>";
+			echo "<td>$cart_item[rental_start]-$cart_item[rental_start]</td>";
+
+			echo "</tr>";
+			
+
+			}
+		echo"</table>"
+
+	?>
