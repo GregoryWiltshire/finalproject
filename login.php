ff175cc6519a7b7e89e45b619839a74d6d2de8ff
DESKTOP-IPOUBQ5\Vyas <svyas2@student.gsu.edu>
2018-12-09 15:53:50 -0500

    login registration css pages working

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ session_start();
 <html>
 <head>
   <title>Login</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="test1.css">
 </head>
 <body>
@@ -44,7 +45,7 @@ session_start();
 ?>
   <div class="container" id="test2">
   <form id="contact" action="" method="post">
-    <h1>Login to Play</h1>
+    <h1>Login</h1>
     <fieldset>
     <fieldset>
      <?php echo $logattempts; ?>
